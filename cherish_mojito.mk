5c512e6b3e2b5300be61c4791f67ae808691e963
dopaemon <polarisdp@gmail.com>
2022-09-10 16:19:45 +0000

    CherishOS: Initial Build [ Unofficial Type ]
    Signed-off-by: dopaemon <polarisdp@gmail.com>

--- cherish_mojito.mk
+++ cherish_mojito.mk
@@ -11,18 +11,27 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some Evolution X stuffs
+# Inherit some CherishOS stuffs
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/evolution/config/common_full_phone.mk)
+$(call inherit-product, vendor/cherish/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := evolution_mojito
+PRODUCT_NAME := cherish_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Cherish_Magic
+CHERISH_BUILD_TYPE := UNOFFICIAL
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.cherish.maintainer=@KernelPanix
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+WITH_GMS := true
